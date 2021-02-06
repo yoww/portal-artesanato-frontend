a598d1a2bdd25f91b0797466847d7b07097bf7d6
Ruan Lima <ruan.slima@gmail.com>
2021-02-06 19:38:24 -0300

    chore: criação da arquitetura/boilerplate

---
+++ app/components/modules/Header/Header.tsx
@@ -0,0 +1,7 @@
+import Icon, { Icons } from '@element/Icon/Icon';
+import styles from './Header.module.scss';
+
+const Header = () => (
+);
+
+export default Header;
---
+++ app/components/templates/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import styles from './HomePage.module.scss';
+
+const HomePage = () => (
+  
+);
+
+export default HomePage;
